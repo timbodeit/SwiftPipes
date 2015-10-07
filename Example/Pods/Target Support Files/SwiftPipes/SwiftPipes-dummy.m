b6c84122d5bbd04f079222766b88d4e1f7b53f3e
Tim Bodeit <tim@bodeit.com>
2015-10-07 13:33:43 +0200

    Fix .gitignore and remove Pods directory

--- Example/Pods/Target Support Files/SwiftPipes/SwiftPipes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftPipes : NSObject
-@end
-@implementation PodsDummy_SwiftPipes
-@end
--- Example/Pods/Target Support Files/SwiftPipes/SwiftPipes-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SwiftPipesVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftPipesVersionString[];
-
